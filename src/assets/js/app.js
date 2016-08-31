c68f27d18b197d570eb38508e39afadd3ac637f4
Andy Gauge <andygauge@gmail.com>
2016-08-30 21:05:24 -0700

    top navigation generated, image support, and responsive

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,7 @@
 $(document).foundation();
+$(document).ready(function() {
+  $(window).scroll(function() {
+    $('#menu-chevron').css('top', $(document).scrollTop())
+  })
+  $('#menu-chevron').click(function() { window.scroll(0,0) } )
+})
